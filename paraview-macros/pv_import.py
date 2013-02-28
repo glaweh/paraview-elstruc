f047e20ea44222fb53de59824269fbe937aee46d
Henning Glawe <glaweh@debian.org>
2013-02-28 16:55:47 +0100

    split up into functions

--- paraview-macros/pv_import.py
+++ paraview-macros/pv_import.py
@@ -1,138 +1,177 @@
-sourcedict = GetSources()
-if (len(sourcedict) != 1):
-    raise RuntimeError('Only the density file should be loaded')
-density_vtk = sourcedict.values()[0]
-SetActiveSource(density_vtk)
-RenameSource("Input Densities")
-
+from   paraview.simple import *
+import PyQt4.QtGui
 import re
-(filebase, filebase_rel) = \
-    re.compile("^(.*/([^/]+))-[^/-]+\.vtk$").match(density_vtk.FileNames[0]).groups()
-at_vtk = LegacyVTKReader(FileNames= filebase + '-atoms.vtk',registrationName="Input Atoms")
-at_vtk.UpdatePipeline()
-at_vtk.UpdatePipelineInformation()
-density_vtk.UpdatePipeline()
-density_vtk.UpdatePipelineInformation()
-GetDisplayProperties(density_vtk).Representation='Outline'
-Hide(density_vtk)
+import os
 
-t=Transform(at_vtk)
-GetDisplayProperties(t).Representation='Outline'
-Hide(t)
-atoms_trans = [t]
+if (len(GetSources()) != 0):
+    PyQt4.QtGui.QMessageBox.critical(None,'Error','Please start with a clean pipeline')
+    raise RuntimeError('Please start with a clean pipeline')
 
-atoms_group=GroupDatasets(registrationName="Atoms")
-GetDisplayProperties(atoms_group).Representation='Outline'
-atoms_group.Input=atoms_trans
-Hide(atoms_group)
-SetActiveSource(atoms_group)
-CovalentSpheres=Glyph(atoms_group,GlyphType='Sphere',registrationName="Covalent Spheres")
-CovalentSpheres.Scalars='Covalent_radius'
-CovalentSpheres.ScaleMode = 'scalar'
-CovalentSpheres.SetScaleFactor=1
-CovalentSpheres.UpdatePipeline
-CovalentSpheres.GlyphType.PhiResolution=16
-CovalentSpheres.GlyphType.ThetaResolution=16
-CovalentSpheres.GlyphType.Radius=1
-dp=GetDisplayProperties(CovalentSpheres)
-dp.LookupTable = GetLookupTableForArray("Nuclear_charge", 1,
-        RGBPoints = [1, 0, 0, 1, 100, 1, 0, 0],
-        ColorSpace = "HSV")
-dp.ColorAttributeType = 'POINT_DATA'
-dp.ColorArrayName = 'Nuclear_charge'
-dp.Opacity = 0.4
-Hide(CovalentSpheres)
-SetActiveSource(atoms_group)
-Nuclei = Glyph(atoms_group,GlyphType='Sphere',registrationName="Nuclei")
-dp=GetDisplayProperties(Nuclei)
-dp.LookupTable = GetLookupTableForArray("Nuclear_charge", 1,
-        RGBPoints = [1, 0, 0, 1, 100, 1, 0, 0],
-        ColorSpace = "HSV")
-dp.ColorAttributeType = 'POINT_DATA'
-dp.ColorArrayName = 'Nuclear_charge'
-Show(Nuclei)
+filebase = ''
 
-t=Transform(density_vtk)
-GetDisplayProperties(t).Representation='Outline'
-Hide(t)
-density_trans=[t]
+def ImportEspressoPP():
+    density_filename=str(PyQt4.QtGui.QFileDialog.getOpenFileName(None,
+            "Select density file",
+            '',
+            "All files (*.*);;VTK (*.vtk *.VTK)",
+            "VTK (*.vtk *.VTK)"))
+    global filebase
+    (filebase, filebase_rel) = \
+        re.compile("^(.*/([^/]+))-[^/-]+\.vtk$").match(density_filename).groups()
+    density_vtk = LegacyVTKReader(FileNames= density_filename ,registrationName="Input Densities")
+    density_vtk.UpdatePipeline()
+    density_vtk.UpdatePipelineInformation()
+    GetDisplayProperties(density_vtk).Representation='Outline'
+    Hide(density_vtk)
+    at_vtk = LegacyVTKReader(FileNames= filebase + '-atoms.vtk',registrationName="Input Atoms")
+    at_vtk.UpdatePipeline()
+    at_vtk.UpdatePipelineInformation()
+    GetDisplayProperties(at_vtk).Representation='Outline'
+    Hide(at_vtk)
+    return filebase
 
-densities=density_vtk.GetDataInformation().DataInformation.GetPointDataInformation()
-dos_offset = -42.0
-calc_function = 'd_vol'
-offset_pattern = re.compile('^([pm])(\d+\.\d+)_dos$')
-for i in range(0,densities.GetNumberOfArrays()):
-    this_dos_name   = densities.GetArrayInformation(i).GetName()
-    m = offset_pattern.match(this_dos_name)
-    if (m == None):
-        continue
-    this_dos_offset = float(m.group(2))
-    if (m.group(1) == 'm'):
-        this_dos_offset = -this_dos_offset
-    dao = abs(this_dos_offset) - abs(dos_offset)
-    if (dao > 0.001):
-        continue
-    elif (dao > -0.001):
-        if (this_dos_offset > 0):
-            continue
-    dos_offset = this_dos_offset
-    calc_function = this_dos_name + '/d_vol'
+def SetupAtomPipeline():
+    at_vtk = FindSource("Input Atoms")
+    if (at_vtk == None):
+        raise RuntimeError('Missing "Input Atoms"')
+    SetActiveSource(at_vtk)
+    t=Transform(at_vtk)
+    GetDisplayProperties(t).Representation='Outline'
+    t.UpdatePipeline()
+    t.UpdatePipelineInformation()
+    Hide(t)
+    atoms_group=GroupDatasets(registrationName="Atoms")
+    GetDisplayProperties(atoms_group).Representation='Outline'
+    atoms_group.Input=[t]
+    Hide(atoms_group)
+    atoms_group.UpdatePipeline()
+    atoms_group.UpdatePipelineInformation()
+    atoms_clip = Clip(atoms_group,registrationName="Clip")
+    atoms_clip.ClipType='Box'
+    atoms_clip.UpdatePipeline()
+    atoms_clip.UpdatePipelineInformation()
+    GetDisplayProperties(atoms_clip).Representation='Outline'
+    Hide(atoms_clip)
+    SetActiveSource(atoms_clip)
+    CovalentSpheres=Glyph(atoms_clip,GlyphType='Sphere',registrationName="Covalent Spheres")
+    CovalentSpheres.Scalars='Covalent_radius'
+    CovalentSpheres.ScaleMode = 'scalar'
+    CovalentSpheres.SetScaleFactor=1
+    CovalentSpheres.GlyphType.PhiResolution=16
+    CovalentSpheres.GlyphType.ThetaResolution=16
+    CovalentSpheres.GlyphType.Radius=1
+    dp=GetDisplayProperties(CovalentSpheres)
+    dp.LookupTable = GetLookupTableForArray("Nuclear_charge", 1,
+            RGBPoints = [1, 0, 0, 1, 100, 1, 0, 0],
+            ColorSpace = "HSV")
+    dp.ColorAttributeType = 'POINT_DATA'
+    dp.ColorArrayName = 'Nuclear_charge'
+    dp.Opacity = 0.4
+    Hide(CovalentSpheres)
+    SetActiveSource(atoms_clip)
+    Nuclei = Glyph(atoms_clip,GlyphType='Sphere',registrationName="Nuclei")
+    dp=GetDisplayProperties(Nuclei)
+    dp.LookupTable = GetLookupTableForArray("Nuclear_charge", 1,
+            RGBPoints = [1, 0, 0, 1, 100, 1, 0, 0],
+            ColorSpace = "HSV")
+    dp.ColorAttributeType = 'POINT_DATA'
+    dp.ColorArrayName = 'Nuclear_charge'
+    Show(Nuclei)
 
-density_group=GroupDatasets(registrationName="Densities")
-GetDisplayProperties(density_group).Representation='Outline'
-density_group.Input=density_trans
-Hide(density_group)
+def SetupDensityPipeline():
+    density_vtk = FindSource("Input Densities")
+    if (density_vtk == None):
+        raise RuntimeError('Missing "Input Densities"')
+    SetActiveSource(density_vtk)
+    t=Transform(density_vtk)
+    GetDisplayProperties(t).Representation='Outline'
+    t.UpdatePipeline()
+    t.UpdatePipelineInformation()
+    Hide(t)
+    density_group=GroupDatasets(registrationName="Densities")
+    GetDisplayProperties(density_group).Representation='Outline'
+    density_group.Input=[t]
+    Hide(density_group)
+    density_group.UpdatePipeline()
+    density_group.UpdatePipelineInformation()
+    calc_function=DefaultDensity(density_group)
+    if (calc_function == ''):
+        calc_function = 'd_vol'
+    else:
+        calc_function = calc_function + '/d_vol'
+    SetActiveSource(density_group)
+    DCalc = Calculator(density_group,registrationName="Density Calculator",
+            ResultArrayName = "CalcDensity", Function = calc_function)
+    DCalc.UpdatePipeline()
+    DCalc.UpdatePipelineInformation()
+    GetDisplayProperties(DCalc).Representation='Outline'
+    Show(DCalc)
+    SetupDensitySlices(DCalc,"CalcDensity")
 
-SetActiveSource(density_group)
-DCalc = Calculator(density_group,registrationName="Density Calculator",
-        ResultArrayName = "CalcDensity", Function = calc_function)
-DCalc.UpdatePipeline()
-DCalc.UpdatePipelineInformation()
-GetDisplayProperties(DCalc).Representation='Outline'
-Show(DCalc)
-
-heatmap = [
-   0.00000000000000000, 0.0, 0.0, 0.0,
-   0.19999999999999999, 0.9019607843137255, 0.0, 0.0,
-   0.39999999999999997, 0.9019607843137255, 0.9019607843137255, 0.0,
-   0.50000000000000000, 1.0, 1.0, 1.0]
-
-calcrange = DCalc.GetDataInformation().DataInformation.GetPointDataInformation().GetArrayInformation('CalcDensity').GetComponentRange(0)
-
-scale = (calcrange[1]-calcrange[0])/(heatmap[12]-heatmap[0])
-heatmap[0]  = calcrange[0]
-heatmap[12] = calcrange[1]
-heatmap[4]  = calcrange[0]+heatmap[4]*scale
-heatmap[8]  = calcrange[0]+heatmap[8]*scale
-
-# loop over slices
-lt = GetLookupTableForArray( "CalcDensity", 1, Discretize=1,
+def SetupDensitySlices(data,field):
+    heatmap = [
+       0.00000000000000000, 0.0, 0.0, 0.0,
+       0.19999999999999999, 0.9019607843137255, 0.0, 0.0,
+       0.39999999999999997, 0.9019607843137255, 0.9019607843137255, 0.0,
+       0.50000000000000000, 1.0, 1.0, 1.0]
+    calcrange = data.GetDataInformation().DataInformation.GetPointDataInformation().GetArrayInformation('CalcDensity').GetComponentRange(0)
+    scale = (calcrange[1]-calcrange[0])/(heatmap[12]-heatmap[0])
+    heatmap[0]  = calcrange[0]
+    heatmap[12] = calcrange[1]
+    heatmap[4]  = calcrange[0]+heatmap[4]*scale
+    heatmap[8]  = calcrange[0]+heatmap[8]*scale
+    lt = GetLookupTableForArray( field, 1, Discretize=1,
         RGBPoints=heatmap,
         UseLogScale=0, VectorComponent=0, NanColor=[0.0, 0.4980392156862745, 1.0],
         NumberOfTableValues=256, ColorSpace='RGB',
         VectorMode='Component',
         HSVWrap=0, ScalarRangeInitialized=1.0,
         LockScalarRange=1 )
+    # loop over slices
+    f = open(filebase + '-cutplanes.tsv')
+    i = 0
+    for line in f:
+        sp = map(float,line.split())
+        SetActiveSource(data)
+        i=i+1
+        s=Slice(data,registrationName="Plane%d" % i)
+        s.SliceType.Normal=[ sp[0],sp[1],sp[2] ]
+        s.SliceType.Origin=[ sp[3],sp[4],sp[5] ]
+        s.UpdatePipeline()
+        s.UpdatePipelineInformation()
+        Hide(s)
+        Render()
+        dp=GetDisplayProperties(s)
+        dp.LookupTable = lt
+        dp.ColorAttributeType = 'POINT_DATA'
+    #    dp.ColorArrayName = field
+        Show(s)
 
-import os
-f = open(filebase + '-cutplanes.tsv')
-i = 0
-for line in f:
-    sp = map(float,line.split())
-    SetActiveSource(DCalc)
-    i=i+1
-    s=Slice(DCalc,registrationName="Plane%d" % i)
-    s.SliceType.Normal=[ sp[0],sp[1],sp[2] ]
-    s.SliceType.Origin=[ sp[3],sp[4],sp[5] ]
-    s.UpdatePipeline()
-    s.UpdatePipelineInformation()
-    Hide(s)
-    Render()
-    dp=GetDisplayProperties(s)
-    dp.LookupTable = lt
-    dp.ColorAttributeType = 'POINT_DATA'
-#    dp.ColorArrayName = 'CalcDensity'
-    Show(s)
+def DefaultDensity(density_group):
+    densities=density_group.GetDataInformation().DataInformation.GetPointDataInformation()
+    result = ''
+    dos_offset = -42.0
+    offset_pattern = re.compile('^([pm])(\d+\.\d+)_dos$')
+    for i in range(0,densities.GetNumberOfArrays()):
+        this_dos_name   = densities.GetArrayInformation(i).GetName()
+        m = offset_pattern.match(this_dos_name)
+        if (m == None):
+            continue
+        this_dos_offset = float(m.group(2))
+        if (m.group(1) == 'm'):
+            this_dos_offset = -this_dos_offset
+        dao = abs(this_dos_offset) - abs(dos_offset)
+        if (dao > 0.001):
+            continue
+        elif (dao > -0.001):
+            if (this_dos_offset > 0):
+                continue
+        dos_offset = this_dos_offset
+        result = this_dos_name
+    return result
 
+ImportEspressoPP()
+SetupAtomPipeline()
+SetupDensityPipeline()
 SetActiveSource(None)
 Render()
